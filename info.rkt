120931ed1b1062d9c27a71727cb049bd7d244736
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-07-11 13:43:45 +0100

    Update info.rkt typo

--- info.rkt
+++ info.rkt
@@ -3,10 +3,10 @@
 (define collection "coroutines")
 
 (define scribblings ; https://docs.racket-lang.org/raco/setup-info.html
-  (("coroutines.scrbl" ; collection-relative path to documentation source
-    () ; flags - defaults to single page
-    ("Concurrency") ; category in docs.racket-lang.org homepage 
-    "coroutines"))) ; name
+   '(("coroutines.scrbl" ; collection-relative path to documentation source
+      () ; flags - defaults to single page
+      ("Concurrency") ; category in docs.racket-lang.org homepage 
+      "coroutines"))) ; name
 (define license
   '(Apache-2.0 OR MIT))
 
